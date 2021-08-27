f79d83225347e6a664d0a1880f37d202e3239701
gip69 <gip@me.com>
2021-08-27 11:33:26 +0200

    set hooks

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'hello',
@@ -7,4 +7,24 @@ import { Component, Input } from '@angular/core';
 })
 export class HelloComponent {
   @Input() name: string;
+
+  constructor() {
+    console.log(this.name); // undefined
+  }
+
+  // changes is an object with a list of keys from type string
+  // and the values are SimpleChange objects
+  ngOnChanges(changes: { [propertyName: string]: SimpleChange }) {
+    // name is set ==> change detected
+    console.log(changes.name);
+    // changes = { 'name': { currentValue: 'Angular 12', previousValue: {} } }
+  }
+
+  ngOnInit() {
+    console.log('on init HelloComponent: ' + this.name);
+  }
+
+  ngOnDestroy() {
+    console.log('on destroy: ' + this.name);
+  }
 }
